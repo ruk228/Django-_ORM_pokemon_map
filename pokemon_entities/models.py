e840a9c23322a826ab83173cf4d9b66c3c7e477d
ruk228 <icecreamcrusher21@gmail.com>
2022-07-26 19:38:56 +0300

    cahnged blank and null

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -5,27 +5,27 @@ from django.utils.timezone import localtime
 # your models here
 
 class Pokemon(models.Model):
-    name = models.TextField(max_length=25, blank=True)
-    name_en = models.TextField(max_length=25, blank=True)
-    name_jp = models.TextField(max_length=25, blank=True)
-    image = models.ImageField(blank=True)
+    name = models.TextField(null=True, max_length=25)
+    name_en = models.TextField(null=True, max_length=25)
+    name_jp = models.TextField(null=True, max_length=25)
+    image = models.ImageField(null=True)
 
     def __str__(self):
         return '{}'.format(self.name)
 
 
 class PokemonEntity(models.Model):
-    author = models.ForeignKey('Pokemon', verbose_name='Покемон', blank=True, on_delete=models.CASCADE, related_name='pokemon')
-    description = models.TextField(null=True, verbose_name='Описание')
-    lat = models.FloatField(null=True, verbose_name='Широта')
+    author = models.ForeignKey('Pokemon', null=True, verbose_name='Покемон', on_delete=models.CASCADE, related_name='pokemon')
+    description = models.TextField(blank=True, verbose_name='Описание')
+    lat = models.FloatField(blank=True, verbose_name='Широта')
     low = models.FloatField(null=True, verbose_name='Долгота')
-    appeared_at = models.DateTimeField(null=True, verbose_name='Время_включения')
-    disappeared_at = models.DateTimeField(null=True, verbose_name='Время_отключения')
-    level = models.IntegerField(null=True, verbose_name='Уровень')
-    health = models.IntegerField(null=True, verbose_name='Здоровье')
-    strength = models.IntegerField(null=True, verbose_name='Прочность')
-    defencer = models.IntegerField(null=True, verbose_name='Защита')
-    stamina = models.IntegerField(null=True, verbose_name='стамина')
+    appeared_at = models.DateTimeField(blank=True, verbose_name='Время_включения')
+    disappeared_at = models.DateTimeField(blank=True, verbose_name='Время_отключения')
+    level = models.IntegerField(blank=True, verbose_name='Уровень')
+    health = models.IntegerField(blank=True, verbose_name='Здоровье')
+    strength = models.IntegerField(blank=True, verbose_name='Прочность')
+    defencer = models.IntegerField(blank=True, verbose_name='Защита')
+    stamina = models.IntegerField(blank=True, verbose_name='стамина')
 
     def __str__(self):
         return '{}'.format(self.author)
